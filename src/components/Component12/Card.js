eaebd45cd303446ba420df8c34e9e2c46c22be0d
unknown <arfinchowdhuryarif7@gmail.com>
2022-01-16 08:05:17 +0600

    some changes

--- src/components/Component12/Card.js
+++ src/components/Component12/Card.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import nerdImage from './assets/illustration.png';
 import Button from './Button';
 // import Content from './Content';
 import Image from './Image';
 import { ButtonContainer, CardContainer, Content } from './styles/Container.style';
 import { H1, P, Tag } from './styles/Elements';
+
+
+
+
+// const array = [23,545,45,55,45];
+// const myFuntion = (initialValue,value,index,array) =>{
+//     // console.log(value)
+//     // console.log(index)
+//     // console.log(array)
+//     // console.log('---------------------------------------------')
+
+//     return  initialValue + value;
+// }
+
+// const result = array.reduce(myFuntion);
+// console.log('result-----', result);
+
+
+
 const Card = () => {
+    const Theme = useContext(ThemeContext);
+
+        console.log(Theme)
     return (
         <CardContainer>
             <Content>
@@ -17,6 +39,7 @@ const Card = () => {
                 <ButtonContainer>
                     <Button link="https://lwsbd.link/rsc" text="Watch now" />
                     <Button link="https://lwsbd.link/react" text="Github repo" />
+                    <Button onClick={()=>{}} text='Change Theme'/>
                 </ButtonContainer>
 
             </Content>
--- src/components/Component12/Component12.js
+++ src/components/Component12/Component12.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './assets/css/dark.css';
 import Tutorial from './Tutorial';
+
 const Component12 = () =>{
+    // const [theme, setTheme] = useState('dark');
+
+    // const switchTheme = () =>{
+    //     if(theme === 'dark'){
+    //         setTheme('dark')
+    //     }
+    //     else{
+    //         setTheme('light')
+    //     }
+
+    // }
+    
+   export const UserContext = React.createContext('light');
+
     return(
         <div>
+            <UserContext.Provider  value={'light'}>
             <Tutorial/>
+            </UserContext.Provider>
         </div>
     )
 }
